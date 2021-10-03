094b3a70658fc5ad501197a1e78317aaea26b393
Alex4422 <alexandregautier82@gmail.com>
2021-10-03 11:39:52 +0200

    initial structure of the DAPP OK

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,107 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import  React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Whitelist from "./contracts/Whitelist.json";
+import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+    //initialisations
+    state = { web3: null, accounts: null, contract: null, whitelist: null};
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+    componentWillMount = async () => {
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+        try {
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+            //get the web3 provider
+            const web3 = await getWeb3();
+
+            //using of web3 to get the accounts of the user (in metamask)
+            const accounts = await web3.eth.getAccounts();
+
+            //get the instance of the smart contract "Whitelist" with web3 and the informations of deployed file (client/src/contracts/Whitelist.json)
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = Whitelist.networks[networkId];
+
+            const instance = new web3.eth.Contract(
+                Whitelist.abi,
+                deployedNetwork && deployedNetwork.address,
+            );
+
+            // Set web3, accounts, and contract to the state, and then proceed with an
+            // example of interacting with the contract's methods.
+            this.setState({web3, accounts, contract: instance}, this.runInit);
+        } catch (error) {
+            // Catch any errors for any of the above operations
+            alert(
+                `Non-Ethereum browser detected. Can you please try to install MetaMask before starting.`,
+            );
+            console.error(error);
+        }
+    };
+
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+    runInit = async() => {
+        const { accounts, contract } = this.state;
+
+        //Get the authorised account list
+        const whitelist = await contract.methods.getAddresses().call();
+
+        //update the state
+        this.setState({whitelist: whitelist});
+    };
+
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+    whitelist = async() => {
+        const { accounts, contract} = this.state;
+        const address = this.address.value;
+
+        //Interaction with the smart contract to add an account
+        await contract.methods.whitelist(address).send({from: accounts[0]});
+
+        //get the list of authorised account list
+        this.runInit();
     }
-  };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+    render(){
+        const { whitelist } = this.state;
+        if (!this.state.web3) {
+            return <div>Loading Web3, accounts, and contract...</div>;
+        }
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+        return (
+
+            <div className="App">
+                Hi, this is Alex!
+
+
+
+
+
+
+
+            </div>
+
+
+        )
+
+
     }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
+
 }
 
 export default App;
