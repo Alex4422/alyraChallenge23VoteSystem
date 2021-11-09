d3fed11d22efc699e1afc667269c4f8a21393fd7
Alex <alexandregautier82@gmail.com>
2021-11-09 16:07:22 +0100

    Updates

---
+++ precache-manifest.bf98ba2d058b3bee9cbadb095149e05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c879e246e5720b07b78a04009210cc9",
+    "url": "/alyraChallenge23VoteSystem/index.html"
+  },
+  {
+    "revision": "cd163136467d8f962b82",
+    "url": "/alyraChallenge23VoteSystem/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "51a184289f3d3c69e84f",
+    "url": "/alyraChallenge23VoteSystem/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cd163136467d8f962b82",
+    "url": "/alyraChallenge23VoteSystem/static/js/2.dfef6cad.chunk.js"
+  },
+  {
+    "revision": "51a184289f3d3c69e84f",
+    "url": "/alyraChallenge23VoteSystem/static/js/main.871dd522.chunk.js"
+  },
+  {
+    "revision": "77db53204c2405caf39c",
+    "url": "/alyraChallenge23VoteSystem/static/js/runtime-main.11d13b95.js"
+  }
+]);
